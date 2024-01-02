31c46fa380e0d0f29a2dc39d4f3bc6291fc7a04c
Flora <h.flora.lin@gmail.com>
2024-01-02 18:27:29 +0100

    make my github look pretty

--- script.js
+++
@@ -1,6 +0,0 @@
-// Add JavaScript functionality as needed
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    // Handle form submission logic here
-    alert('Form submitted!');
-});
